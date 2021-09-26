976824963f4bdee6e784516a2576c7ba1efd65e8
ipinia <josemtzipina@gmail.com>
2021-09-25 21:20:15 -0500

    first commit

---
+++ js/reloj.js
@@ -0,0 +1,43 @@
+const mostrarReloj = () => {
+    let fecha = new Date();
+    let hr = formatoHora(fecha.getHours());
+    let min = formatoHora(fecha.getMinutes());
+    let seg = formatoHora(fecha.getSeconds());
+    document.getElementById("hora").innerHTML = `${hr}:${min}:${seg}`;
+
+    const meses = [
+        "Enero",
+        "Febreo",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "diciembre",
+    ];
+    const dias = [
+        "Domingo",
+        "Lunes",
+        "Martes",
+        "Miércoles",
+        "Jueves",
+        "Viernes",
+        "Sábado",
+    ];
+    let diaSemana = dias[fecha.getDay()];
+    let dia = fecha.getDate();
+    let mes = meses[fecha.getMonth()];
+    let fechaTexto = `${diaSemana}, ${dia} de ${mes}`;
+    document.getElementById("fecha").innerHTML = fechaTexto;
+
+    document.getElementById("contenedor").classList.toggle("animar");
+};
+
+const formatoHora = (hora) => {
+    if (hora < 10) hora = "0" + hora;
+    return hora;
+};
+setInterval(mostrarReloj, 1000);
